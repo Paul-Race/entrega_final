9684b0fc131242cbdeaa8042bc68508be5a8cb70
Paul-Race <136203987+Paul-Race@users.noreply.github.com>
2024-06-26 21:11:06 -0400

    Update settings.py

--- GroundZero/settings.py
+++ GroundZero/settings.py
@@ -162,9 +162,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIR = [BASE_DIR / 'static/']
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 
 # Default primary key field type
